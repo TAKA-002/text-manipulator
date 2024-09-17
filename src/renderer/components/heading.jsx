34a972d6c6dd1d41670790586f5d45fcb9066043
miura takayuki <6015abc@gmail.com>
2024-09-17 20:06:52 +0900

    シンプルに見出しを変更

--- src/renderer/components/heading.jsx
+++ src/renderer/components/heading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Heading() {
   return (
-    <h1 className="text-3xl font-bold text-center text-blue-600 my-6 pb-2 border-b-2 border-blue-200">
+    <h1 className="text-3xl font-bold text-center text-gray-600 mt-6">
       テキスト変換アプリ
     </h1>
   );
