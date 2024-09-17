5784580c9a6fc7249da8123645e18a96112fa0f1
miura takayuki <6015abc@gmail.com>
2024-09-17 20:06:38 +0900

    カラムのgapを微修正

--- src/renderer/components/Container.jsx
+++ src/renderer/components/Container.jsx
@@ -9,7 +9,7 @@ import CopyButton from "./ui/CopyButton";
 export function Container() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="flex flex-col md:flex-row gap-8">
+      <div className="flex flex-col md:flex-row gap-6">
         <div className="w-full md:w-1/2 space-y-6">
           <InputArea />
           <ConversionOptions />
