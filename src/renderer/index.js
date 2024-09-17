a90eea4077f27b95b5ba49a0587a1613c89caab5
miura takayuki <6015abc@gmail.com>
2024-09-17 17:45:23 +0900

    tailwindcssのみの記述に変更。by三浦

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,9 +1,7 @@
 import "./styles/index.css";
-import "./styles/styles.css";
 
 import React from "react";
 import { createRoot } from "react-dom/client";
-
 import { App } from "./components/app";
 
 const root = createRoot(document.getElementById("app"));
