369c2794e9bd187c204236164522580811c03905
miura takayuki <6015abc@gmail.com>
2024-09-18 01:52:32 +0900

    タイトル変更

--- src/renderer/components/heading.jsx
+++ src/renderer/components/heading.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function Heading() {
-  return <h1 className="text-3xl font-bold text-center text-gray-600 mt-6">テキスト変換アプリ</h1>;
+  return <h1 className="text-3xl font-bold text-center text-gray-600 mt-6">Text Manipulation App</h1>;
 }
