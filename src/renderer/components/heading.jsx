448b995a568d5e70fe33c2b42e31d6f0716b1657
miura takayuki <6015abc@gmail.com>
2024-09-18 12:38:11 +0900

    フォーマット実施

--- src/renderer/components/heading.jsx
+++ src/renderer/components/heading.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export function Heading() {
-  return <h1 className="text-3xl font-bold text-center text-gray-600 mt-6">Text Manipulation App</h1>;
+  return (
+    <h1 className="text-3xl font-bold text-center text-gray-600 mt-6">Text Manipulation App</h1>
+  );
 }
