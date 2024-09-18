8807b86f0ab4f1a80e0e19471303f65867b8187c
miura takayuki <6015abc@gmail.com>
2024-09-18 11:35:47 +0900

    開発時必要なソースを追加

--- src/main/main.js
+++ src/main/main.js
@@ -1,13 +1,13 @@
 const { app, BrowserWindow } = require("electron");
-// const electronReloader = require("electron-reloader");
+const electronReloader = require("electron-reloader");
 const path = require("path");
 
-// if (process.env.NODE_ENV === "development") {
-//   electronReloader(module, {
-//     debug: true,
-//     watchRenderer: true,
-//   });
-// }
+if (process.env.NODE_ENV === "development") {
+  electronReloader(module, {
+    debug: true,
+    watchRenderer: true,
+  });
+}
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -23,7 +23,7 @@ function createWindow() {
   win.loadFile(path.join(__dirname, "../../dist/index.html"));
 
   // 開発ツールを開く（開発中は便利だが、製品版ではコメントアウトする）
-  // win.webContents.openDevTools();
+  win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
