3e74add828072e57c81b4f905621dae8350ba36d
miura takayuki <6015abc@gmail.com>
2024-09-21 00:43:51 +0900

    画像の読み込み。by三浦

--- src/renderer/components/heading.jsx
+++ src/renderer/components/heading.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import icon from "../images/m_icon.png";
 
 export function Heading() {
   return (
-    <h1 className="text-3xl font-bold text-center text-gray-600 mt-6">Text Manipulation App</h1>
+    <div>
+      <img src={icon} width="400" height="400" alt="" />
+      <h1 className="text-3xl font-bold text-center text-gray-600 mt-6">Text Manipulation App</h1>
+    </div>
   );
 }
