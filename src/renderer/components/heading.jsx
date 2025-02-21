d1d62e802abe6cdd738e26790d47be5318825ab4
miura takayuki <6015abc@gmail.com>
2025-02-22 08:51:58 +0900

    heading フォントサイズの変更と、画像比率をaspect-ratioで設定

--- src/renderer/components/heading.jsx
+++ src/renderer/components/heading.jsx
@@ -4,16 +4,16 @@ import icon from "../images/m_icon.png";
 export function Heading() {
   return (
     <div className="flex items-center gap-3 justify-center mt-6">
-      <figure className="w-14 h-auto">
+      <figure className="aspect-square max-w-[min(14.9vw,56px)] h-full">
         <img
-          className="w-full h-auto"
+          className="w-full h-full"
           src={icon}
           width="400"
           height="400"
           alt="Text Manipulater App Logo"
         />
       </figure>
-      <h1 className="text-3xl font-bold text-gray-600">Text Manipulater App</h1>
+      <h1 className="text-[min(5.3vw,30px)] font-bold text-gray-600">Text Manipulater App</h1>
     </div>
   );
 }
