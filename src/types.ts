72d8fd50f9633a3d20e18c99113fdc9259468d87
miura takayuki <6015abc@gmail.com>
2025-09-15 11:37:38 +0900

    型エイリアスの順番をapp.tsxと同じように変更

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,5 @@
 // アプリケーション独自の型定義
 
+export type ReplaceObjectType = { from: string; to: string };
 export type DirectionType = "fullToHalf" | "halfToFull";
 export type ToastKindType = "success" | "failed" | "clear" | "";
-export type ReplaceObjectType = { from: string; to: string };
