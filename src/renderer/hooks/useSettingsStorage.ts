3de55931a37fb1a44c2bf64df35d2d3cdc6f47f2
miura takayuki <6015abc@gmail.com>
2025-09-21 16:15:49 +0900

    共通箇所を切り出し実施

--- src/renderer/hooks/useSettingsStorage.ts
+++ src/renderer/hooks/useSettingsStorage.ts
@@ -12,42 +12,39 @@ export const useSettingsStorage = () => {
   const [settingsName, setSettingsName] = useState<string>("");
   const [settingsData, setSettingsData] = useState<ConvertSettings[] | null>(null);
 
+  const getSettingsListFromStorage = () => {
+    const settingsList = getSettingsList();
+    if (settingsList === null) return null;
+    return JSON.parse(settingsList)
+  }
+
   // 初期読み込み
   useEffect(() => {
-    const settingsList = getSettingsList();
-
-    if (settingsList !== null) {
-      const listData = JSON.parse(settingsList);
-      setSettingsData(listData);
-    }
+    const storageData = getSettingsListFromStorage()
+    if (storageData) setSettingsData(storageData);
   }, []);
 
   const saveSettings = (curSettings: SettingsType): void => {
-
     if (settingsName.trim() === "") {
       alert("設定名を入力してください。");
       return;
     }
 
-    const convertSettings: ConvertSettings = {
+    const mergedSettings = getMergedSettings(getSettingsList(), {
       id: createId(),
       name: settingsName,
       settings: curSettings,
       createdAt: formatDateTime(Date.now()),
-    };
-
-    const settingsList = getSettingsList();
-    const mergedSettings = getMergedSettings(settingsList, convertSettings);
+    });
     setLocalStorage(mergedSettings);
     setSettingsData(mergedSettings);
   }
 
   const deleteSettings = (id: string): void => {
-    const settingsList = getSettingsList();
-    if (settingsList === null) return;
+    const data = getSettingsListFromStorage()
+    if (!data) return;
 
-    const parsedData = JSON.parse(settingsList);
-    const deletedData = parsedData.filter((data: ConvertSettings) => data.id !== id);
+    const deletedData = data.filter((d: ConvertSettings) => d.id !== id);
     setLocalStorage(deletedData);
     setSettingsData(deletedData);
   }
