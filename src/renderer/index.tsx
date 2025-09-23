196de3bdd6bbd3189b9d457935e4e2826fda6b7f
miura takayuki <6015abc@gmail.com>
2025-09-23 10:37:38 +0900

    app.tsxをApp.tsxへリネーム

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -2,7 +2,7 @@ import "./styles/index.css";
 
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./components/app";
+import { App } from "./components/App";
 
 const appEl = document.getElementById("app") as HTMLDivElement | null;
 
