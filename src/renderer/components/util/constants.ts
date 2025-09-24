84991729cbe02d29f721a4b3a1778b3a29fba5c3
miura takayuki <6015abc@gmail.com>
2025-09-25 00:10:28 +0900

    最新更新日を明示

--- src/renderer/components/util/constants.ts
+++ src/renderer/components/util/constants.ts
@@ -1,4 +1,5 @@
-export const APP_VERSION = "1.4.0";
+export const APP_VERSION = "1.4.1";
+export const LATEST_UPDATE_DAY = "2025-09-25";
 export const TYPING_DONE_INTERVAL = 700;
 export const TEXT_MANIPULATOR_SETTINGS_KEY = "Text-Manipulator-settings";
 
