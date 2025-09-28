e953f4ce0ae3d2c8cc7e23a51d3eda663799b2ed
miura takayuki <6015abc@gmail.com>
2025-09-28 19:21:54 +0900

    testコードを修正。次はテストケースを増やしてみる。

--- src/renderer/components/tests/utils/process.test.ts
+++ src/renderer/components/tests/utils/process.test.ts
@@ -1,10 +1,12 @@
 import { performReplace } from "../../util/process";
 
-test('文字列が置換できているか', () => {
+describe('文字列が置換できているか？', () => {
+  it('4が100に置換できているか？', () => {
+    const input = "あいうえお4";
+    const replaceRule = { from: '4', to: "100" }
+    const actual = performReplace(input, replaceRule);
+    const expected = 'あいうえお100';
 
-  const testStr = "あいうえお4";
-  const replaceObject = { from: '4', to: "100" }
-
-  const result = performReplace(testStr, replaceObject);
-  expect(result).toBe('あいうえお100');
+    expect(actual).toBe(expected);
+  });
 })
