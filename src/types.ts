93bb5b55c2255e6c0ab58ec34433e1ddf37d0033
miura takayuki <miura@infocity.co.jp>
2026-02-24 12:02:24 +0900

    ユニオン型で大文字小文字変換機能のoptionを型定義

--- src/types.ts
+++ src/types.ts
@@ -3,6 +3,7 @@
 export type ReplaceObjectType = { from: string; to: string };
 export type DirectionType = "fullToHalf" | "halfToFull";
 export type ToastKindType = "success" | "failed" | "clear" | "";
+export type CaseConversionType ="none" | "upper" | "lower" | "title";
 
 export interface SettingsType {
   replaceObject: ReplaceObjectType,
