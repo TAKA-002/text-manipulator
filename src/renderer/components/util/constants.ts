27e74ba961c03dceeab10120832f70326698b887
miura takayuki <miura@infocity.co.jp>
2026-02-24 12:03:50 +0900

    定数で変換型を定義し、Recordでvalueの方を紐づけてCaseConversionType以外のものはエラーになるように設定

--- src/renderer/components/util/constants.ts
+++ src/renderer/components/util/constants.ts
@@ -1,5 +1,13 @@
+import { CaseConversionType } from "../../../types";
+
 export const APP_VERSION = "1.4.2";
 export const LATEST_UPDATE_DAY = "2026-02-21";
 export const TYPING_DONE_INTERVAL = 700;
 export const TEXT_MANIPULATOR_SETTINGS_KEY = "Text-Manipulator-settings";
+export const CASE_CONVERSION_TYPES: Record<string, CaseConversionType> = {
+  NO_CONVERT: "none",
+  UPPER_CASE: "upper",
+  LOWER_CASE: "lower",
+  TITLE_CASE: "title"
+}
 
